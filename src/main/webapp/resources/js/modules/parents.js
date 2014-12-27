5d8c05b9ce3933fe24b6eb6e6e1b9fbb5835d347
benweizhu <benweizhu@gmail.com>
2014-12-28 00:41:30 +0800

    [Relations] [Benwei] 添加 parentsService

--- src/main/webapp/resources/js/modules/parents.js
+++ src/main/webapp/resources/js/modules/parents.js
@@ -1,9 +1,33 @@
-angular.module("parentsApp", [])
-    .controller("parentsController", function ($scope) {
+angular.module('parentsApp', [])
+    .factory('parentsService', function () {
+        return {
+            getLocusCodes: function () {
+                var locusCodes = ['just', 'put', 'something', 'here'];
+                return locusCodes;
+            },
+            getPi: function () {
+                var pi = null;
+                return pi;
+            }
+        }
+
+    })
+    .controller('parentsController', function ($scope, parentsService) {
         $scope.locuses = [];
-        $scope.locusCodes = [];
+        $scope.locusCodes = parentsService.getLocusCodes();
+
         $scope.add = function () {
-            $scope.locuses.push({code: $scope.code, af1: $scope.af1, af2: $scope.af2,
-                m1: $scope.m1, m2: $scope.m2, c1: $scope.c1, c2: $scope.c2, pi: ''});
+            $scope.locuses.push(
+                {
+                    code: $scope.code,
+                    af1: $scope.af1,
+                    af2: $scope.af2,
+                    m1: $scope.m1,
+                    m2: $scope.m2,
+                    c1: $scope.c1,
+                    c2: $scope.c2,
+                    pi: parentsService.getPi()
+                }
+            );
         }
     });
