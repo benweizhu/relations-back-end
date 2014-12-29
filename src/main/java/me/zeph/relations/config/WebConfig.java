577eb650349a2ee47c3121106a0dc71d2795f45b
benweizhu <benweizhu@gmail.com>
2014-12-29 16:52:12 +0800

    [Relations] [Benwei] 改用@Configuration来做依赖管理，添加unused import和文件长度的checkstyle

---
+++ src/main/java/me/zeph/relations/config/WebConfig.java
@@ -0,0 +1,42 @@
+package me.zeph.relations.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import static org.springframework.http.MediaType.TEXT_HTML;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "me.zeph.relations")
+public class WebConfig {
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+		internalResourceViewResolver.setViewClass(JstlView.class);
+		internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+		internalResourceViewResolver.setSuffix(".jsp");
+		return internalResourceViewResolver;
+	}
+
+	@Bean
+	public ContentNegotiationManagerFactoryBean contentNegotiationManagerFactoryBean() {
+		ContentNegotiationManagerFactoryBean contentNegotiation = new ContentNegotiationManagerFactoryBean();
+		contentNegotiation.setFavorPathExtension(false);
+		contentNegotiation.setFavorParameter(false);
+		contentNegotiation.setDefaultContentType(TEXT_HTML);
+		return contentNegotiation;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource resourceBundleMessageSource() {
+		ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+		resourceBundleMessageSource.setBasenames("AGCU_EX22", "AGCU211");
+		return resourceBundleMessageSource;
+	}
+}
