5cbe6850c592c8f8a23c5dc33aff494721939778
benweizhu <benweizhu@gmail.com>
2014-12-30 21:35:48 +0800

    [Relations] [Benwei] 添加AlleleValueService来获取Allele对应的真正值

--- src/main/java/me/zeph/relations/service/AlleleValueService.java
+++ src/main/java/me/zeph/relations/service/AlleleValueService.java
@@ -1,11 +1,30 @@
 package me.zeph.relations.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
+
+import static java.lang.Float.valueOf;
+import static java.util.Locale.getDefault;
 
 @Service
 public class AlleleValueService {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	public AlleleValueService() {
+	}
+
+	public AlleleValueService(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+
 	public float getValue(String kit, String locus, float allele) {
-		return 0;
+		return valueOf(applicationContext.getMessage(getAllele(kit, locus, allele), null, getDefault()));
+	}
+
+	private String getAllele(String kit, String locus, float allele) {
+		return kit + "." + locus + "." + allele;
 	}
 }
---
+++ src/test/resources/me/zeph/relations/service/AlleleValueServiceTest.java
@@ -0,0 +1,22 @@
+package me.zeph.relations.service;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AlleleValueServiceTest {
+	@Test
+	public void shouldTranslateStringToFloat() {
+		ApplicationContext mockContext = mock(ApplicationContext.class);
+		when(mockContext.getMessage(anyString(), (Object[]) anyObject(), (Locale) anyObject())).thenReturn("0.001");
+
+		assertEquals(0.001f, new AlleleValueService(mockContext).getValue("kit", "locus", 1), 0.0001);
+	}
+}
