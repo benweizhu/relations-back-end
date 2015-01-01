f9635d58d4fcdba3f4161cc588697aa15bd6f1b5
benweizhu <benweizhu@gmail.com>
2015-01-01 19:20:35 +0800

    [Relations] [Benwei] add pi service client call logic

--- src/main/webapp/resources/js/modules/parents.js
+++ src/main/webapp/resources/js/modules/parents.js
@@ -1,18 +1,6 @@
 angular.module('parentsApp', ['restApp'])
-    .factory('parentsService', function () {
-        return {
-            getLocusCodes: function () {
-                var locusCodes = ['just', 'put', 'something', 'here'];
-                return locusCodes;
-            },
-            getPi: function () {
-                var pi = null;
-                return pi;
-            }
-        };
-    })
-    .controller('parentsController', function ($scope, locusRest, kitRest) {
-        $scope.locuses = [];
+    .controller('parentsController', function ($scope, locusRest, kitRest, piRest) {
+
         kitRest.get({}, function (data) {
             $scope.kits = data.kits;
         });
@@ -26,17 +14,16 @@ angular.module('parentsApp', ['restApp'])
         };
 
         $scope.add = function () {
-            $scope.locuses.push(
-                {
-                    code: $scope.code,
-                    af1: $scope.af1,
-                    af2: $scope.af2,
-                    m1: $scope.m1,
-                    m2: $scope.m2,
-                    c1: $scope.c1,
-                    c2: $scope.c2,
-                    pi: parentsService.getPi()
-                }
-            );
+
+            var req = {kit: $scope.kit, locus: $scope.code, af1: $scope.af1, af2: $scope.af2, m1: $scope.m1,
+                m2: $scope.m2, c1: $scope.c1, c2: $scope.c2};
+
+            piRest.calculateParentsPi(req, function (resp) {
+                $scope.locuses = [];
+                $scope.locuses.push({code: $scope.code, af1: $scope.af1, af2: $scope.af2, m1: $scope.m1,
+                    m2: $scope.m2, c1: $scope.c1, c2: $scope.c2, pi: resp.value});
+            });
+
         };
+
     });
--- src/main/webapp/resources/js/modules/rest.js
+++ src/main/webapp/resources/js/modules/rest.js
@@ -3,5 +3,11 @@ angular.module('restApp', ['ngResource'])
         return $resource('/relations-with-gradle/kits');
     })
     .factory('locusRest', function ($resource) {
-        return $resource("/relations-with-gradle/locus?kit=:kit");
+        return $resource('/relations-with-gradle/locus?kit=:kit');
+    })
+    .factory('piRest', function ($resource) {
+        return $resource('/relations-with-gradle/pi/:action', {}, {
+            calculateParentsPi: {method: 'POST', params: { action: "parents" }, isArray: false },
+            calculateOneParentPi: {method: 'POST', params: { action: "oneparent" }, isArray: false }
+        });
     });
