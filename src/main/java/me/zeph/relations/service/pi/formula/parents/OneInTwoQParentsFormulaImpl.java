9ff36adaf380155daf119f295af56bfc414f09ba
benweizhu <benweizhu@gmail.com>
2015-01-02 19:49:15 +0800

    [Relations] [Benwei] create Parents Formula class and add test case pdf to git

---
+++ src/main/java/me/zeph/relations/service/pi/formula/parents/OneInTwoQParentsFormulaImpl.java
@@ -0,0 +1,12 @@
+package me.zeph.relations.service.pi.formula.parents;
+
+import me.zeph.relations.model.ParentsReqParam;
+import me.zeph.relations.service.pi.formula.ParentsFormula;
+
+public class OneInTwoQParentsFormulaImpl extends ParentsFormula {
+	@Override
+	public double calculate(double c1Value, double c2Value, double m1Value, double m2Value,
+	                        double af1Value, double af2Value, ParentsReqParam reqParam) {
+		return 0;
+	}
+}
