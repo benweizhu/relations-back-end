8c059167a8660111b3310a89200794d8e6cfc9dc
benweizhu <benweizhu@gmail.com>
2015-01-05 12:43:01 +0800

    [Relations] [Benwei] change the way of adding dependencies in angularjs

--- src/main/webapp/resources/js/modules/rest.js
+++ src/main/webapp/resources/js/modules/rest.js
@@ -1,13 +1,13 @@
 angular.module('restApp', ['ngResource'])
-    .factory('kitRest', function ($resource) {
+    .factory('kitRest', ['$resource', function ($resource) {
         return $resource('/relations-with-gradle/kits');
-    })
-    .factory('locusRest', function ($resource) {
+    }])
+    .factory('locusRest', ['$resource', function ($resource) {
         return $resource('/relations-with-gradle/locus?kit=:kit');
-    })
-    .factory('piRest', function ($resource) {
+    }])
+    .factory('piRest', ['$resource', function ($resource) {
         return $resource('/relations-with-gradle/pi/:action', {}, {
             calculateParentsPi: {method: 'POST', params: { action: "parents" }, isArray: false },
             calculateOneParentPi: {method: 'POST', params: { action: "oneparent" }, isArray: false }
         });
-    });
+    }]);
