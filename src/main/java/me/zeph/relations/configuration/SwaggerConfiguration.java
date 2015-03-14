ff6f7c397f3819701357de859ec198a61465da97
benweizhu <benweizhu@gmail.com>
2015-03-14 15:28:48 +0800

    [Relations-Back-End][Benwei] enable swagger remove @enablewebmvc to allow spring boot default configuration

---
+++ src/main/java/me/zeph/relations/configuration/SwaggerConfiguration.java
@@ -0,0 +1,8 @@
+package me.zeph.relations.configuration;
+
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableSwagger
+public class SwaggerConfiguration {}
--- src/main/java/me/zeph/relations/configuration/WebContextConfiguration.java
+++ src/main/java/me/zeph/relations/configuration/WebContextConfiguration.java
@@ -5,10 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = "me.zeph.relations")
 public class WebContextConfiguration {
 
