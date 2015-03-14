65dd6efdb251c5c94cfa056104384f9c4b6371d3
benweizhu <benweizhu@gmail.com>
2015-03-14 14:28:22 +0800

    [Relations-Back-End][Benwei] remove useless bean since spring boot provide default ones

--- src/main/java/me/zeph/relations/configuration/WebContextConfiguration.java
+++ src/main/java/me/zeph/relations/configuration/WebContextConfiguration.java
@@ -5,12 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
-
-import static org.springframework.http.MediaType.TEXT_HTML;
 
 @Configuration
 @EnableWebMvc
@@ -21,24 +16,6 @@ public class WebContextConfiguration {
 	private static final String AGCU_211 = "AGCU211";
 	private static final String KITS = "Kits";
 	private static final String EXPRESSION = "EXPRESSION";
-
-	@Bean
-	public InternalResourceViewResolver internalResourceViewResolver() {
-		InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-		internalResourceViewResolver.setViewClass(JstlView.class);
-		internalResourceViewResolver.setPrefix("/WEB-INF/view/");
-		internalResourceViewResolver.setSuffix(".jsp");
-		return internalResourceViewResolver;
-	}
-
-	@Bean
-	public ContentNegotiationManagerFactoryBean contentNegotiationManagerFactoryBean() {
-		ContentNegotiationManagerFactoryBean contentNegotiation = new ContentNegotiationManagerFactoryBean();
-		contentNegotiation.setFavorPathExtension(false);
-		contentNegotiation.setFavorParameter(false);
-		contentNegotiation.setDefaultContentType(TEXT_HTML);
-		return contentNegotiation;
-	}
 
 	@Bean
 	public ResourceBundleMessageSource messageSource() {
