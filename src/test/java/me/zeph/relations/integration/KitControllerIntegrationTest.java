380940f2f3a8520862d07207dfe7ca60808777be
benweizhu <benweizhu@gmail.com>
2015-03-18 19:17:45 +0800

    [relations-back-end][benwei] fix check style

--- src/test/java/me/zeph/relations/integration/KitControllerIntegrationTest.java
+++ src/test/java/me/zeph/relations/integration/KitControllerIntegrationTest.java
@@ -13,11 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
