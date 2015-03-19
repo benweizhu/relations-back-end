85d03a1c2659e3fd2739a666c9af138b9c3255b4
benweizhu <benweizhu@gmail.com>
2015-03-19 23:30:30 +0800

    [relations-back-end][benwei] add error handle for allele

---
+++ src/main/java/me/zeph/relations/advice/AlleleControllerAdvice.java
@@ -0,0 +1,33 @@
+package me.zeph.relations.advice;
+
+import me.zeph.relations.controller.AlleleController;
+import me.zeph.relations.exception.AlleleNotFoundException;
+import me.zeph.relations.exception.KitNotFoundException;
+import me.zeph.relations.model.api.ApiError;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
+@ControllerAdvice(assignableTypes = {AlleleController.class})
+@Order(HIGHEST_PRECEDENCE)
+public class AlleleControllerAdvice {
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(value = AlleleNotFoundException.class)
+	@ResponseBody
+	public ApiError handleAlleleNotFoundException(AlleleNotFoundException e) {
+		return new ApiError(e.getMessage());
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(value = KitNotFoundException.class)
+	@ResponseBody
+	public ApiError handleKitNotFoundException(KitNotFoundException e) {
+		return new ApiError(e.getMessage());
+	}
+}
