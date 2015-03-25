cdfe3a06d6c1d8667037e2eae9c9e7079f87d27a
benweizhu <benweizhu@gmail.com>
2015-03-25 20:02:29 +0800

    [relations-back-end][benwei] change database model: user case, one to many, case items, one to many

--- config/database/relations_create.sql
+++ config/database/relations_create.sql
@@ -5,18 +5,40 @@ CREATE TABLE relr_kit_allele (
   CONSTRAINT relr_kit_allele_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE relt_locus_probability (
-  id          INT            NOT NULL AUTO_INCREMENT,
-  allele_id   INT            NOT NULL,
-  locus       DECIMAL(10, 5) NOT NULL,
-  probability DECIMAL(10, 7) NOT NULL,
-  CONSTRAINT relt_locus_probability_pk PRIMARY KEY (id)
+CREATE TABLE relr_parents (
+  id        INT            NOT NULL AUTO_INCREMENT,
+  case_id   INT            NOT NULL,
+  allele_id INT            NOT NULL,
+  af_1      DECIMAL(10, 5) NOT NULL,
+  af_2      DECIMAL(10, 5) NOT NULL,
+  m_1       DECIMAL(10, 5) NOT NULL,
+  m_2       DECIMAL(10, 5) NOT NULL,
+  c_1       DECIMAL(10, 5) NOT NULL,
+  c_2       DECIMAL(10, 5) NOT NULL,
+  CONSTRAINT relr_parents_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE relr_single_parent (
+  id        INT            NOT NULL AUTO_INCREMENT,
+  case_id   INT            NOT NULL,
+  allele_id INT            NOT NULL,
+  af_1      DECIMAL(10, 5) NOT NULL,
+  af_2      DECIMAL(10, 5) NOT NULL,
+  c_1       DECIMAL(10, 5) NOT NULL,
+  c_2       DECIMAL(10, 5) NOT NULL,
+  CONSTRAINT relr_single_parent_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE relt_allele (
   id   INT         NOT NULL AUTO_INCREMENT,
   name VARCHAR(20) NOT NULL,
   CONSTRAINT relt_allele_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE relt_case (
+  id      INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  CONSTRAINT relt_case_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE relt_kit (
@@ -31,33 +53,22 @@ CREATE TABLE relt_location (
   CONSTRAINT relt_location_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE relr_user_case (
-  case_id INT         NOT NULL,
-  user    VARCHAR(10) NOT NULL,
-  CONSTRAINT relr_user_case_pk PRIMARY KEY (case_id)
+CREATE TABLE relt_locus_probability (
+  id          INT            NOT NULL AUTO_INCREMENT,
+  allele_id   INT            NOT NULL,
+  locus       DECIMAL(10, 5) NOT NULL,
+  probability DECIMAL(10, 7) NOT NULL,
+  CONSTRAINT relt_locus_probability_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE relt_parents (
-  id      INT            NOT NULL AUTO_INCREMENT,
-  case_id INT            NOT NULL,
-  af_1    DECIMAL(10, 5) NOT NULL,
-  af_2    DECIMAL(10, 5) NOT NULL,
-  m_1     DECIMAL(10, 5) NOT NULL,
-  m_2     DECIMAL(10, 5) NOT NULL,
-  c_1     DECIMAL(10, 5) NOT NULL,
-  c_2     DECIMAL(10, 5) NOT NULL,
-  CONSTRAINT relt_parents_pk PRIMARY KEY (id)
+CREATE TABLE relt_user (
+  id   INT         NOT NULL AUTO_INCREMENT,
+  name VARCHAR(10) NOT NULL,
+  CONSTRAINT relt_user_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE relt_single_parent (
-  id      INT            NOT NULL AUTO_INCREMENT,
-  case_id INT            NOT NULL,
-  af_1    DECIMAL(10, 5) NOT NULL,
-  af_2    DECIMAL(10, 5) NOT NULL,
-  c_1     DECIMAL(10, 5) NOT NULL,
-  c_2     DECIMAL(10, 5) NOT NULL,
-  CONSTRAINT relt_single_parent_pk PRIMARY KEY (id)
-);
+ALTER TABLE relt_case ADD CONSTRAINT case_fk_user FOREIGN KEY case_fk_user (user_id)
+REFERENCES relt_user (id);
 
 ALTER TABLE relr_kit_allele ADD CONSTRAINT ka_fk_allele FOREIGN KEY ka_fk_allele (allele_id)
 REFERENCES relt_allele (id);
@@ -68,9 +79,9 @@ REFERENCES relt_kit (id);
 ALTER TABLE relt_locus_probability ADD CONSTRAINT rlp_fk_allele FOREIGN KEY rlp_fk_allele (allele_id)
 REFERENCES relt_allele (id);
 
-ALTER TABLE relt_parents ADD CONSTRAINT rp_fk_case_id FOREIGN KEY rp_fk_case_id (case_id)
-REFERENCES relr_user_case (case_id);
+ALTER TABLE relr_parents ADD CONSTRAINT rp_fk_case FOREIGN KEY rp_fk_case (case_id)
+REFERENCES relt_case (id);
 
-ALTER TABLE relt_single_parent ADD CONSTRAINT rsp_fk_case_id FOREIGN KEY rsp_fk_case_id (case_id)
-REFERENCES relr_user_case (case_id);
+ALTER TABLE relr_single_parent ADD CONSTRAINT rsp_fk_case FOREIGN KEY rsp_fk_case (case_id)
+REFERENCES relt_case (id);
 
--- config/database/relations_drop.sql
+++ config/database/relations_drop.sql
@@ -1,14 +1,16 @@
+ALTER TABLE relt_case DROP FOREIGN KEY case_fk_user;
 ALTER TABLE relr_kit_allele DROP FOREIGN KEY ka_fk_allele;
 ALTER TABLE relr_kit_allele DROP FOREIGN KEY ka_fk_kit;
 ALTER TABLE relt_locus_probability DROP FOREIGN KEY rlp_fk_allele;
-ALTER TABLE relt_parents DROP FOREIGN KEY rp_fk_case_id;
-ALTER TABLE relt_single_parent DROP FOREIGN KEY rsp_fk_case_id;
+ALTER TABLE relr_parents DROP FOREIGN KEY rp_fk_case;
+ALTER TABLE relr_single_parent DROP FOREIGN KEY rsp_fk_case;
 
 DROP TABLE relr_kit_allele;
-DROP TABLE relt_locus_probability;
+DROP TABLE relr_parents;
+DROP TABLE relr_single_parent;
 DROP TABLE relt_allele;
+DROP TABLE relt_case;
 DROP TABLE relt_kit;
 DROP TABLE relt_location;
-DROP TABLE relt_single_parent;
-DROP TABLE relt_parents;
-DROP TABLE relr_user_case;
+DROP TABLE relt_locus_probability;
+DROP TABLE relt_user;
