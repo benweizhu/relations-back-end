2e71c07989baf3dd551147166411b72b462e71ed
benweizhu <benweizhu@gmail.com>
2015-03-25 15:36:01 +0800

    [relations-back-end][benwei] change database model to record each user's cases

--- config/database/relations_create.sql
+++ config/database/relations_create.sql
@@ -32,6 +32,34 @@ CREATE TABLE relt_location (
   CONSTRAINT relt_location_pk PRIMARY KEY (id)
 );
 
+CREATE TABLE relr_user_case (
+  case_id INT         NOT NULL,
+  user    VARCHAR(10) NOT NULL,
+  CONSTRAINT relr_user_case_pk PRIMARY KEY (case_id)
+);
+
+CREATE TABLE relt_parents (
+  id      INT            NOT NULL AUTO_INCREMENT,
+  case_id INT            NOT NULL,
+  af_1    DECIMAL(10, 5) NOT NULL,
+  af_2    DECIMAL(10, 5) NOT NULL,
+  m_1     DECIMAL(10, 5) NOT NULL,
+  m_2     DECIMAL(10, 5) NOT NULL,
+  c_1     DECIMAL(10, 5) NOT NULL,
+  c_2     DECIMAL(10, 5) NOT NULL,
+  CONSTRAINT relt_parents_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE relt_single_parent (
+  id      INT            NOT NULL AUTO_INCREMENT,
+  case_id INT            NOT NULL,
+  af_1    DECIMAL(10, 5) NOT NULL,
+  af_2    DECIMAL(10, 5) NOT NULL,
+  c_1     DECIMAL(10, 5) NOT NULL,
+  c_2     DECIMAL(10, 5) NOT NULL,
+  CONSTRAINT relt_single_parent_pk PRIMARY KEY (id)
+);
+
 ALTER TABLE relr_kit_allele ADD CONSTRAINT ka_fk_allele FOREIGN KEY ka_fk_allele (allele_id)
 REFERENCES relt_allele (id);
 
@@ -43,4 +71,10 @@ REFERENCES relt_allele (id);
 
 ALTER TABLE relr_loca_alle_locus ADD CONSTRAINT lal_fk_location FOREIGN KEY lal_fk_location (location_id)
 REFERENCES relt_location (id);
+
+ALTER TABLE relt_parents ADD CONSTRAINT rp_fk_case_id FOREIGN KEY rp_fk_case_id (case_id)
+REFERENCES relr_user_case (case_id);
+
+ALTER TABLE relt_single_parent ADD CONSTRAINT rsp_fk_case_id FOREIGN KEY rsp_fk_case_id (case_id)
+REFERENCES relr_user_case (case_id);
 
--- config/database/relations_drop.sql
+++ config/database/relations_drop.sql
@@ -2,10 +2,14 @@ ALTER TABLE relr_kit_allele DROP FOREIGN KEY ka_fk_allele;
 ALTER TABLE relr_kit_allele DROP FOREIGN KEY ka_fk_kit;
 ALTER TABLE relr_loca_alle_locus DROP FOREIGN KEY lal_fk_allele;
 ALTER TABLE relr_loca_alle_locus DROP FOREIGN KEY lal_fk_location;
+ALTER TABLE relt_parents DROP FOREIGN KEY rp_fk_case_id;
+ALTER TABLE relt_single_parent DROP FOREIGN KEY rsp_fk_case_id;
 
 DROP TABLE relr_kit_allele;
 DROP TABLE relr_loca_alle_locus;
 DROP TABLE relt_allele;
 DROP TABLE relt_kit;
 DROP TABLE relt_location;
-
+DROP TABLE relt_single_parent;
+DROP TABLE relt_parents;
+DROP TABLE relr_user_case;
