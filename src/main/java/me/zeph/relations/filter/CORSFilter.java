cab4bc9645a88751efc7c958fb21a5ece5637425
benweizhu <benweizhu@gmail.com>
2015-04-18 20:57:04 +0800

    [relations-back-end][benwei] refactoring

--- src/main/java/me/zeph/relations/filter/CORSFilter.java
+++ src/main/java/me/zeph/relations/filter/CORSFilter.java
@@ -7,11 +7,6 @@ import java.io.IOException;
 public class CORSFilter implements Filter {
 
 	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-
-	}
-
-	@Override
 	public void doFilter(ServletRequest request, ServletResponse req, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) req;
 		response.setHeader("Access-Control-Allow-Origin", "*");
@@ -19,6 +14,11 @@ public class CORSFilter implements Filter {
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Content-Type");
 		chain.doFilter(request, req);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
 	}
 
 	@Override
